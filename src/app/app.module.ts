454849ec239a7f087c400971e6ab7f6e9e5a460a
elsker14 <iancujianu98@gmail.com>
2022-06-07 16:09:13 +0300

    section 12: search products by category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
+import {Routes, RouterModule} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +22,7 @@ import {ProductService} from "./services/product.service";
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
@@ -24,5 +34,7 @@ export class AppModule { }
 
 /*
 Info:
-  Daca punem ProductService in Provider ne va permite sa folosim si in alte parti clasa
+  Daca punem ProductService in Provider ne va permite sa folosim si in alte parti clasa.
+
+  Ceea ce am facut aici a fost sa scriem rutele posibile de URL-uri si sa le adaugam pentru a putea fi folosite peste tot.
  */
