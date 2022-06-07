8743665fca2670c1a4831ed0e7b94095f080335a
elsker14 <iancujianu98@gmail.com>
2022-06-07 18:03:45 +0300

    section 13: done, dar am eroare la afisarea categoriilor

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -23,18 +23,17 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId)
-    {
-      // get the "id" param string, and convert string to a number using the "+" symbol
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       // @ts-ignore
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     }
-    else
-    {
-      // not category id available...default to category id 1
+    else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
 
