bf94a6a356efc9654aef3170228464ae9ff2331d
elsker14 <iancujianu98@gmail.com>
2022-06-07 18:48:49 +0300

    section 13: am gasit eroarea, era c mic, in loc de C mare la categoryUrl in product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,7 @@ import {ProductCategory} from "../common/product-category";
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products?size=100';
-  private categoryUrl = 'http://localhost:8080/api/product-Category';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
