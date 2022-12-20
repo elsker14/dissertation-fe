b0981161415f185e2994a3fa6be91a9673fca100
Iancu Jianu <iancujianu98@gmail.com>
2022-12-20 15:55:47 +0200

    section 17: part 120 - created cart-status component

--- src/app/components/product-list/product-list.component.spec.ts
+++ src/app/components/product-list/product-list.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "../../services/product.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {NgbPagination} from "@ng-bootstrap/ng-bootstrap";
+import {spyOnClass} from 'jasmine-es6-spies'
+import {of} from "rxjs";
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -8,7 +15,11 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductListComponent ]
+      declarations: [ ProductListComponent, NgbPagination],
+      imports: [HttpClientModule, HttpClientTestingModule, RouterTestingModule ],
+      providers: [
+        {provide: ProductService, useFactory: () => spyOnClass(ProductService) }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +32,13 @@ describe('ProductListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find category name on the page', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="category-name"]')).toBeTruthy();
+  });
+
+  it('should show a list of products on the page', ()=>{
+    expect(fixture.nativeElement.querySelector('[data-test="products-list"]')).toBeTruthy();
   });
 });
