b72723754ebf2cc6284e58e0fd3450d96a32651d
Iancu Jianu <iancujianu98@gmail.com>
2022-12-20 17:07:45 +0200

    section 17: part 131-> 132: 1. Add click handler for "Add to cart" button on product-details.component.html 2. Update Product

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../../common/product";
 import {ProductService} from "../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
+import {CartService} from "../../services/cart.service";
+import {CartItem} from "../../common/cart-item";
 
 const PROD_ID = 'prodId';
 const CAT_ID = 'catId';
@@ -16,7 +18,9 @@ export class ProductDetailsComponent implements OnInit {
   product: Product = new Product();
   categoryId: number;
 
-  constructor(private productService: ProductService, private route:ActivatedRoute) { }
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.categoryId = +this.route.snapshot.paramMap.get(CAT_ID)!;
@@ -36,5 +40,11 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     );
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name} -> ${this.product.unitPrice}`)
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
