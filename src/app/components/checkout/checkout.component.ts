e1bbdc63841dfecb8cd365018cffed9dc086428c
Iancu Jianu <iancujianu98@gmail.com>
2022-12-20 21:39:34 +0200

    section 20: part 168 -> 173: populate countries and states Steps: 1. Create TS classes for: Country and State 2. Add methods to the form service for countries and states 3. Update checkout component to retrieve the countries from service 4. Update HTML template to populate drop-down lists for countries 5. Add Event handler for checkout component 5.1 Read the selected country, retrieve list of states based on country 6. Update HTML template to populate drop-down lists for states

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Luv2ShopFormService} from "../../services/luv2-shop-form.service";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -10,11 +12,14 @@ import {Luv2ShopFormService} from "../../services/luv2-shop-form.service";
 export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   totalPrice: number = 0;
   totalQuantity: number = 0;
   creditCardMonths: number[] = [];
   creditCardYears: number[] = [];
+  countries: Country[] = []
 
 
   constructor(private formBuilder: FormBuilder,
@@ -42,7 +47,7 @@ export class CheckoutComponent implements OnInit {
         zipCode: ['']
       }),
       creditCard: this.formBuilder.group({
-        cartType: [''],
+        cardType: [''],
         nameOnCard: [''],
         cardNumber: [''],
         securityCode: [''],
@@ -69,6 +74,14 @@ export class CheckoutComponent implements OnInit {
         console.log("Retrieved credit card months: " + JSON.stringify(data))
       }
     );
+
+    // populate countries
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    )
   }
 
   onSubmit() {
@@ -108,5 +121,28 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
     )
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`)
+    console.log(`${formGroupName} country name: ${countryName}`)
+
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        // @ts-ignore
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
   }
 }
