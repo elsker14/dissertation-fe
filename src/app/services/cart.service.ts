5faca5e09c1b8edfdc37f0953424f419c94a823e
Iancu Jianu <iancujianu98@gmail.com>
2022-12-21 16:32:01 +0200

    section 27: part 238 -> 242: refresh browser Steps: 1. Update CartService to read data from session storage 2. Add new method in CartService: persistCartItems() 3. Modify computeCartTotals() to call new method: persistCartItems()

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -18,7 +18,25 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
+  // sessionStorage = sesiunea curenta a browserului, daca acesta se inchide, pierdem datele
+  // storage: Storage = sessionStorage;
+  // localStorage = daca browserul se inchide NU pierdem datele
+  storage: Storage = localStorage;
+
   constructor() {
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if(data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotal();
+    }
+  }
+
+  persisCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   addToCart(theCartItem: CartItem) {
@@ -65,6 +83,9 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persisCartItems();
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
