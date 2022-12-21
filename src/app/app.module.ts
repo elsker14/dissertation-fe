e908888f03921995b66d2755117d7870943c337e
Iancu Jianu <iancujianu98@gmail.com>
2022-12-21 20:06:21 +0200

    section 29: part 254 -> 260: order history Steps: 1. keep tracking of logged in user's email with web browser storage 2. create order history class 3. develop order history service 4. generate order history component 5. update template text in html page 6. add orders button to login status component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,12 +25,14 @@ import {
 import {OktaAuth} from "@okta/okta-auth-js";
 import myAppConfig from "./config/my-app-config";
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  {path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginComponent},
@@ -65,7 +67,8 @@ function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
-    MembersPageComponent
+    MembersPageComponent,
+    OrderHistoryComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- src/app/components/login-status/login-status.component.ts
+++ src/app/components/login-status/login-status.component.ts
@@ -9,6 +9,8 @@ import {OktaAuth} from "@okta/okta-auth-js";
 })
 export class LoginStatusComponent implements OnInit {
 
+  storage: Storage = sessionStorage;
+
   isAuthenticated: boolean = false;
   userFullName: string = '';
 
@@ -33,6 +35,12 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res) => {
           this.userFullName = res.name as string;
+
+          // retrieve the user's email from authentication response
+          const theEmail = res.email;
+
+          // now store the email in browser storage
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       );
     }
